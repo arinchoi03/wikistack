d5e3eef14b90c2841ca6aad53f35674fe7100015
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-03-13 14:20:54 -0400

    up to rendering page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var morgan = require("morgan");
 var express = require("express");
-var body_parser = require("body-parser");
+var bodyParser = require("body-parser");
 var nunjucks = require("nunjucks");
 var models = require("./models");
 var chalk = require("chalk");
@@ -9,8 +9,14 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
 
-// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+app.use('/wiki', require('./routes/wiki')); //combines var wikiRouter = require('./routes/wiki); & app.use('/wiki', wikiRouter);
+
+
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
 // instance, which we'll want to use to add Markdown support later.
 var env = nunjucks.configure('views', {noCache: true});
 // have res.render work with html files
@@ -18,10 +24,13 @@ app.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
 
+app.get('/', function(req, res, next) {
+	res.send('Homepage')
+})
 
-models.User.sync({})
+models.User.sync()
 .then(function() {
-	return models.Page.sync({})
+	return models.Page.sync()
 })
 .then(function() {
 	app.listen(3000, function() {
--- models/index.js
+++ models/index.js
@@ -3,14 +3,17 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 	logging: false
 });
 
+//three parts of define 'string', 'models', 'methods'
 var Page = db.define('page', {
+	//MODEL
 	title: {
 		type: Sequelize.STRING,
 		allowNull: false
 	},
 	urlTitle: {
 		type: Sequelize.STRING,
-		allowNull: false
+		allowNull: false,
+		unique: true
 	},
 	content: {
 		type: Sequelize.TEXT,
@@ -18,6 +21,18 @@ var Page = db.define('page', {
 	},
 	status: {
 		type: Sequelize.ENUM('open', 'closed')
+	},
+
+}, { //METHODS
+	getterMethods: {
+		route: function() {
+			return '/wiki/' + this.urlTitle
+		}
+	},
+	hooks: {
+		beforeValidate: function(page) {
+			page.urlTitle = generateUrlTitle(page.title)
+		}
 	}
 });
 
@@ -37,3 +52,14 @@ module.exports = {
 	Page: Page,
 	User: User
 };
+
+function generateUrlTitle (title) {
+  if (title) {
+    // Removes all non-alphanumeric characters from title
+    // And make whitespace underscore
+    return title.replace(/\s+/g, '_').replace(/\W/g, '');
+  } else {
+    // Generates random 5 letter string
+    return Math.random().toString(36).substring(2, 7);
+  }
+}
